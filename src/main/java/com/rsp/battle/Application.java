c451bc784a9299cfd55e8c26bd26a06aa832f3f7
이민선 <woooooooow22@gmail.com>
2026-03-01 17:29:21 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/com/rsp/battle/Application.java
+++ src/main/java/com/rsp/battle/Application.java
@@ -2,19 +2,11 @@ package com.rsp.battle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping("/api/ping")
-	public String ping() {
-		return "pong";
 	}
 }
