26a17c8af129cd58964cdba4b3e4c21423f181c0
이민선 <woooooooow22@gmail.com>
2026-03-01 22:00:05 +0900

    refactor: 테스트 폴더 구조 변경

--- src/test/java/com/rsp/battle/auth/domain/AccessTokenTest.java
+++ src/test/java/com/rsp/battle/auth/domain/AccessTokenTest.java
@@ -1,6 +1,4 @@
-package com.rsp.battle.auth.jwt;
-
-import com.rsp.battle.auth.domain.AccessToken;
+package com.rsp.battle.auth.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
