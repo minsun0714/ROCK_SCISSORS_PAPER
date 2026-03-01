e4552dd5aa64a7ce961ed19b41a14928d201dc6d
이민선 <woooooooow22@gmail.com>
2026-03-01 17:40:36 +0900

    feat: OAuth2 state 및 AuthorizationRequest Redis 기반 관리 구현
    - CustomAuthorizationRequestResolver 구현
      - app_redirect_uri 파라미터 추출 및 state 기반 Redis 저장
    - OAuth2StateRedisService 추가
      - OAuth2AuthorizationRequest 및 redirectUri TTL(3분) 저장/조회/삭제
    - RedisOAuth2AuthorizationRequestRepository 구현
      - state를 쿠키(oauth2_state) + Redis로 관리
      - secure / same-site 설정 프로퍼티화
    - OAuth 전용 RedisTemplate(oauthRedisTemplate) 구성
      - String key + JDK 직렬화 value 설정

---
+++ src/main/java/com/rsp/battle/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.rsp.battle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "oauthRedisTemplate")
+    public RedisTemplate<String, Object> oauthRedisTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
