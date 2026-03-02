4a377e0ad561b9b9bdcbf9e5b4e3a0f3859d4ae9
이민선 <woooooooow22@gmail.com>
2026-03-02 19:40:58 +0900

    refactor: 상태메시지 변경 DTO의 record명 변경

--- src/main/java/com/rsp/battle/user/presentation/StatusMessageUpdateResponse.java
+++ src/main/java/com/rsp/battle/user/presentation/StatusMessageUpdateResponse.java
@@ -2,13 +2,13 @@ package com.rsp.battle.user.presentation;
 
 import com.rsp.battle.user.domain.User;
 
-public record UserProfileResponse(
+public record StatusMessageUpdateResponse(
         Long id,
         String nickname,
         String statusMessage
 ) {
-    public static UserProfileResponse from(User user) {
-        return new UserProfileResponse(
+    public static StatusMessageUpdateResponse from(User user) {
+        return new StatusMessageUpdateResponse(
                 user.getId(),
                 user.getNickname(),
                 user.getStatusMessage()
