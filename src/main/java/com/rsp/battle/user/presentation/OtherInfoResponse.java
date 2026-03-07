ca302a5d8cb3de41e9ddc373fdaf3c4f2b052e9e
이민선 <woooooooow22@gmail.com>
2026-03-07 12:25:15 +0900

    fix: response dto에서 profileImageKey로 잘못 되어있던 필드를 profileImageUrl로 변경

--- src/main/java/com/rsp/battle/user/presentation/OtherInfoResponse.java
+++ src/main/java/com/rsp/battle/user/presentation/OtherInfoResponse.java
@@ -6,7 +6,7 @@ import com.rsp.battle.user.domain.User;
 public record OtherInfoResponse(
         Long userId,
         String nickname,
-        String profileImageKey,
+        String profileImageUrl,
         String statusMessage,
         PresenceStatus presenceStatus,
         FriendStatus friendStatus
